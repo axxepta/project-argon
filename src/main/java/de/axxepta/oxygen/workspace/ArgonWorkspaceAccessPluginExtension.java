49a2fa28cecf4aa186ab8535a68ac395e5b09843
MarkusWb <wima771@gmx.de>
2016-05-30 17:36:26 +0200

    Extra thread for query validation, added interface for connection with Word (preliminary)

--- src/main/java/de/axxepta/oxygen/workspace/ArgonWorkspaceAccessPluginExtension.java
+++ src/main/java/de/axxepta/oxygen/workspace/ArgonWorkspaceAccessPluginExtension.java
@@ -33,6 +33,7 @@ import javax.swing.tree.TreeSelectionModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.io.*;
+import java.lang.reflect.InvocationTargetException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.*;
@@ -140,7 +141,7 @@ public class ArgonWorkspaceAccessPluginExtension implements WorkspaceAccessPlugi
                          */
                         @Override
                         public void filterValidationProblems(ValidationProblems validationProblems) {
-                            // get content of editor window
+/*                            // get content of editor window
                             String editorContent;
                             logger.debug("filter validation problems");
                             try {
@@ -173,7 +174,30 @@ public class ArgonWorkspaceAccessPluginExtension implements WorkspaceAccessPlugi
                                         new DocumentPositionedInfo(DocumentPositionedInfo.SEVERITY_ERROR, valProbStr.get(2), "",
                                                 Integer.parseInt(valProbStr.get(0)), Integer.parseInt(valProbStr.get(1)), 0);
                                 problemList.add(dpi);
+                            }*/
+
+                            /*try {
+                                SwingUtilities.invokeAndWait(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        problemList = isQueryValid(editorAccess);
+                                    }
+                                });
+                            } catch (InvocationTargetException ite) {
+                                logger.error(ite);
+                            } catch (InterruptedException ie) {
+                                logger.error(ie);
+                            }*/
+
+                            final List<DocumentPositionedInfo> problemList = new ArrayList<>();
+                            Thread validatorThread = new Thread(new QueryValidator(editorAccess, problemList));
+                            validatorThread.run();
+                            try {
+                                validatorThread.join();
+                            } catch (InterruptedException ie) {
+                                // ToDo:
                             }
+
                             //The DocumentPositionInfo represents an error with location in the document and has a constructor like:
                             //  public DocumentPositionedInfo(int severity, String message, String systemID, int line, int column, int length)
                             validationProblems.setProblemsList(problemList);
@@ -467,6 +491,94 @@ public class ArgonWorkspaceAccessPluginExtension implements WorkspaceAccessPlugi
                 viewInfo.setTitle("Argon Version History");
             }
         }
+    }
+
+    private static class QueryValidator implements Runnable {
+
+        private final WSEditor editorAccess;
+        private final List<DocumentPositionedInfo> problemList;
+
+        public QueryValidator(WSEditor editorAccess, List<DocumentPositionedInfo> problemList) {
+            this.editorAccess = editorAccess;
+            this.problemList = problemList;
+        }
+
+        public void run() {
+            // get content of editor window
+            String editorContent;
+            logger.debug("filter validation problems");
+            try {
+                InputStream editorStream = editorAccess.createContentInputStream();
+                Scanner s = new java.util.Scanner(editorStream, "UTF-8").useDelimiter("\\A");
+                editorContent = s.hasNext() ? s.next() : "";
+                editorStream.close();
+            } catch (IOException er) {
+                logger.error(er);
+                editorContent = "";
+            }
+            // pass content of editor window to BaseXRequest with queryTest
+            ArrayList<String> valProbStr;
+            try {
+                BaseXRequest testQuery = new BaseXRequest("parse",
+                        BaseXSource.DATABASE, editorContent);
+                valProbStr = testQuery.getResult();
+            } catch (Exception er) {
+                logger.error("query to BaseX failed");
+                valProbStr = new ArrayList<>();
+                valProbStr.add("1");
+                valProbStr.add("1");
+                valProbStr.add("Fatal BaseX request error: "+er.getMessage());
+                er.printStackTrace();
+            }
+            // build DocumentPositionedInfo list from query return;
+            if (valProbStr.size() > 0) {
+                DocumentPositionedInfo dpi =
+                        new DocumentPositionedInfo(DocumentPositionedInfo.SEVERITY_ERROR, valProbStr.get(2), "",
+                                Integer.parseInt(valProbStr.get(0)), Integer.parseInt(valProbStr.get(1)), 0);
+                problemList.add(dpi);
+            }
+            //wait(1000);
+        }
+
+    }
+
+    private List<DocumentPositionedInfo> isQueryValid(WSEditor editorAccess) {
+
+        // get content of editor window
+        String editorContent;
+        logger.debug("filter validation problems");
+        try {
+            InputStream editorStream = editorAccess.createContentInputStream();
+            Scanner s = new java.util.Scanner(editorStream, "UTF-8").useDelimiter("\\A");
+            editorContent = s.hasNext() ? s.next() : "";
+            editorStream.close();
+        } catch (IOException er) {
+            logger.error(er);
+            editorContent = "";
+        }
+        // pass content of editor window to BaseXRequest with queryTest
+        ArrayList<String> valProbStr;
+        try {
+            BaseXRequest testQuery = new BaseXRequest("parse",
+                    BaseXSource.DATABASE, editorContent);
+            valProbStr = testQuery.getResult();
+        } catch (Exception er) {
+            logger.error("query to BaseX failed");
+            valProbStr = new ArrayList<>();
+            valProbStr.add("1");
+            valProbStr.add("1");
+            valProbStr.add("Fatal BaseX request error: "+er.getMessage());
+            er.printStackTrace();
+        }
+        // build DocumentPositionedInfo list from query return;
+        List<DocumentPositionedInfo> problemList = new ArrayList<>();
+        if (valProbStr.size() > 0) {
+            DocumentPositionedInfo dpi =
+                    new DocumentPositionedInfo(DocumentPositionedInfo.SEVERITY_ERROR, valProbStr.get(2), "",
+                            Integer.parseInt(valProbStr.get(0)), Integer.parseInt(valProbStr.get(1)), 0);
+            problemList.add(dpi);
+        }
+        return problemList;
     }
 
 }
