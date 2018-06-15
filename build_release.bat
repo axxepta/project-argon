ecdbc0e2c65e23c7bf201fe7c01f0adc52e77e09
mgaerber <gaerber@axxepta.de>
2018-06-15 18:15:34 +0200

    handle right click on mac os

--- build_release.bat
+++ build_release.bat
@@ -1,9 +1,9 @@
-set versionnumber=10
+set versionnumber=11
 copy extension.xml build\release
 copy plugin.xml build\release\project-argon
 del build\release\project-argon\libs\argon*.jar
 xcopy build\libs build\release\project-argon\libs\ /Y/S/I
 xcopy build\resources\main build\release\project-argon\resources /Y/S/I
 cd build\release
-jar cvf Argon-0.0.%versionnumber%.jar project-argon
+jar cvf argon-0.0.%versionnumber%.jar project-argon
 cd ..\..
